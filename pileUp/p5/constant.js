c1ccd82afb0b5cc1ee72192cde71370de7985895
JeukHwang <jeukhwang.dev@gmail.com>
2023-02-01 15:58:59 +0900

    feat: add proejct pile up

---
+++ pileUp/p5/constant.js
@@ -0,0 +1,11 @@
+export const render = {
+    fps: 20,
+    width: 15, height: 15, block: 20,
+    palette: [
+        "888888", // bg
+        "D8F3DC", "B7E4C7", "95D5B2",
+        "74C69D", "52B788", "40916C",
+        "2D6A4F", "1B4332", "081C15",
+    ],
+    stopPossibility: 0.3
+};
