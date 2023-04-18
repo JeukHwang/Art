8ad2a5bc0bb5cbda8986b7cbcdb9a51b4784c9d7
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-04-18 09:10:04 +0900

    feat: blur size change by mouse movement

---
+++ pma/p5/constant.js
@@ -0,0 +1,6 @@
+export const render = {
+    fps: 60,
+    width: 1440, height: 900, scale: 0.5,
+    historyLength: 100,
+    blur: 5
+};
--- pma/p5/sketch.js
+++ pma/p5/sketch.js
@@ -1,32 +1,55 @@
+import { render } from "./constant.js";
+
 const sketch = (p5) => {
+    const width = render.width * render.scale;
+    const height = render.height * render.scale;
     let image;
     let image_blur;
     let image_blur_masked;
     let mask;
-    let width, height;
+    let history = [{ x: width / 2, y: height / 2 }];
 
     p5.preload = () => {
         image = p5.loadImage("../data/wally.jpg");
+        image.resize(width, height);
     };
     p5.setup = () => {
-        width = image.width;
-        height = image.height;
-
         p5.createCanvas(width, height);
         p5.frameRate(60);
         p5.pixelDensity(p5.displayDensity());
 
         image_blur = p5.createImage(width, height);
         image_blur.copy(image, 0, 0, width, height, 0, 0, width, height);
-        image_blur.filter(p5.BLUR, 10);
+        image_blur.filter(p5.BLUR, render.blur);
         image_blur_masked = p5.createImage(width, height);
-    };
-    p5.draw = () => {
-        p5.image(image, 0, 0);
+
         mask = p5.createGraphics(width, height);
-        mask.circle(p5.mouseX, p5.mouseY, 300);
+    };
+
+    const sum = (array) => array.reduce((a, b) => a + b, 0);
+    const pythagoras = (a, b) => (a ** 2 + b ** 2) ** 0.5;
+    p5.draw = () => {
+        const pos = {
+            x: Math.min(Math.max(p5.mouseX, 0), width),
+            y: Math.min(Math.max(p5.mouseY, 0), height)
+        };
+        history.push(pos);
+        if (history.length > render.historyLength) {
+            history.shift();
+        }
+
+        const distance = sum(history.map((pos, i) => {
+            if (i === 0) { return 0; }
+            return pythagoras(pos.x - history[i - 1].x, pos.y - history[i - 1].y);
+        }));
+        const diagonal = pythagoras(width, height);
+        const radius = diagonal - p5.map(distance, 0, 6000, 0, diagonal, true);
+
+        mask.clear();
+        mask.circle(pos.x, pos.y, 2 * radius);
         image_blur_masked.copy(image_blur, 0, 0, width, height, 0, 0, width, height);
         image_blur_masked.mask(mask);
+        p5.image(image, 0, 0);
         p5.image(image_blur_masked, 0, 0);
     };
 };
